14ce4ede1207038eeb89a6d91918a4e8dbc30e73
Nicko <nicko@qsmam.com>
2016-09-13 13:47:51 +0800

    Add project files.

---
+++ ClientScriptLibrary/Scripts/MetaFunctionUtility.js
@@ -0,0 +1,19 @@
+﻿
+// Meta function is an utility class to check for function
+//
+function MetaFunctionUtility() {
+    this.isFunction = function (functionToCheck) {
+        var tempType = {};
+        return functionToCheck && tempType.toString.call(functionToCheck) === '[object Function]';
+    }
+
+    this.isObject = function (functionToCheck) {
+        var tempType = {};
+        return functionToCheck && tempType.toString.call(functionToCheck) === '[object object]';
+    }
+
+    this.isDefinedOrNotNull = function (functionToCheck) {
+        var tempType = {};
+        return functionToCheck !== undefined && functionToCheck !== null;
+    }
+}
---
+++ ClientScriptLibrary/Scripts/WebUserAgentUtility.js
@@ -0,0 +1,27 @@
+﻿// Web user agent utility is an utility class to check container of this website
+
+function WebUserAgentUtility() {
+
+    this.GetContainer = function () {
+        var result = "";
+        if (navigator.userAgent.match(/iPhone/i)) {
+            result = "iphone";
+        } else if (navigator.userAgent.match(/android/i)) {
+            result = "android";
+        } else if (navigator.userAgent.match(/ipad/i)) {
+            result = "ipad";
+        } else if (navigator.userAgent.match(/firefox/i)) {
+            result = "firefox";
+        } else if (navigator.userAgent.match(/msie/i) || navigator.userAgent.match(/trident/i)) {
+            result = "ie";
+        }
+        return result;
+    }
+
+    this.GetVersion = function () {
+        var result = "";
+        result = navigator.appVersion;
+        return result;
+    }
+
+}
